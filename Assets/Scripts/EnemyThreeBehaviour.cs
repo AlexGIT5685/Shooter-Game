f615d2b1c75916e39373441c3cb216e52efdac9b
[mathewmatiks] <[mathewmatiks@gmail.com]>
2023-11-02 19:43:45 -0400

    Added Enemy Three

---
+++ Assets/Scripts/EnemyThreeBehaviour.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+public class EnemyThreeBehaviour : MonoBehaviour
+{
+
+    private float timer = 0.0f;
+
+    void Start()
+    {
+
+
+    }
+
+
+   void Update()
+   {
+        timer += Time.deltaTime;
+
+        transform.Translate(new Vector3(-1, -1, 0) * Time.deltaTime * 3);
+
+        //make coutner for 10-20 frames change Vector3 x from -1 to 1
+        //делить с остатком на 50,
+        if (timer > 0.5f)
+        {
+            transform.Translate(new Vector3(2, -1, 0) * Time.deltaTime * 3);
+        }
+
+        if (timer > 1f)
+        {
+            transform.Translate(new Vector3(-2, -1, 0) * Time.deltaTime * 3);
+        }
+
+        if (timer > 1.5f)
+        {
+            transform.Translate(new Vector3(3, -1, 0) * Time.deltaTime * 3);
+        }
+
+        if (transform.position.y < -8f)
+       {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,12 +6,14 @@ public class GameManager : MonoBehaviour
 {
     public GameObject enemyOnePrefab;
     public GameObject enemyTwoPrefab;
+    public GameObject enemyThreePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 2.0f, 2.5f);
         InvokeRepeating("CreateEnemyTwo", 3.0f, 3f);
+        InvokeRepeating("CreateEnemyThree", 1.0f, 3.0f);
     }
 
     // Update is called once per frame
@@ -28,5 +30,9 @@ public class GameManager : MonoBehaviour
     void CreateEnemyTwo()
     {
         Instantiate(enemyTwoPrefab, new Vector3(Random.Range(-8, 8), Random.Range(7, 9f), 0), Quaternion.identity);
+    }
+    void CreateEnemyThree()
+    {
+        Instantiate(enemyThreePrefab, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.identity);
     }
 }
