250b0677001ab9ac7498bdedda43f09a8cdf65b8
Dormio-san <liam.lovett06@gmail.com>
2023-11-02 17:04:22 -0400

    Fixed PlayerBehavior script and added BulletBehavior script. PlayerBehavior script will have future modifications, but this is a current working version.

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
     public float speed;
+    public float horizontalInput;
+    public float verticalInput;
+    public float horizontalScreenLimit;
+    public float verticalScreenLimit;
+    public GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +32,22 @@ public class PlayerBehavior : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
 
-        if (transform.position.x > horizontalScreenLimit || transform.position.x < -horizontalScreenLimit)
+        if (transform.position.x > horizontalScreenLimit)
         {
             transform.position = new Vector3(-horizontalScreenLimit, transform.position.y, 0);
         }
-        else if{
+        else if (transform.position.x < -horizontalScreenLimit)
+        {
+            transform.position = new Vector3(horizontalScreenLimit, transform.position.y, 0);
+        }
 
+        if (transform.position.y > verticalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x, -verticalScreenLimit, 0);
+        }
+        else if (transform.position.y < -verticalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.y, verticalScreenLimit, 0);
         }
     }
 
