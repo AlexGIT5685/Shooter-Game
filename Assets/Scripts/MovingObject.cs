dcc26fa4c68577c887394db358a5374bd32198cd
[mathewmatiks] <[mathewmatiks@gmail.com]>
2023-11-09 18:04:33 -0500

    Added a health powerup and fixed coin bug

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public int ObjectType;
+    private float timer = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ObjectType == 4)
+        {
+            // The object is the bullet.
+            transform.Translate(Vector3.up * Time.deltaTime * 8f);
+        }
+        else if (ObjectType == 1)
+        {
+            // The object is enemy one.
+            transform.Translate(Vector3.up * Time.deltaTime * 4.5f);
+        }
+        else if (ObjectType == 2)
+        {
+            // The object is enemy two.
+            transform.Translate(Vector3.up * Time.deltaTime * 3f);
+        }
+        else if (ObjectType == 3)
+        {
+            // The object is enemy three.
+            timer += Time.deltaTime;
+
+            transform.Translate(new Vector3(1, 1, 0) * Time.deltaTime * 2f);
+
+            //make coutner for 10-20 frames change Vector3 x from -1 to 1
+            //делить с остатком на 50,
+            if (timer > 1.2f)
+            {
+                transform.Translate(new Vector3(-2, .3f, 0) * Time.deltaTime * 3);
+            }
+
+            if (timer > 2.1f)
+            {
+                transform.Translate(new Vector3(2.5f, .2f, 0) * Time.deltaTime * 3);
+            }
+
+            if (timer > 3f)
+            {
+                transform.Translate(new Vector3(-2.5f, .1f, 0) * Time.deltaTime * 3);
+            }
+
+            if (timer > 3.9f)
+            {
+                transform.Translate(new Vector3(2.5f, .1f, 0) * Time.deltaTime * 3);
+            }
+        }
+        if (ObjectType == 5)
+        {
+            // The object is the coin.            
+            transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * 2.9f);
+
+            if (transform.position.x >= 9.8f)
+            {
+                // When it reaches the edge, go to the opposite side.
+                transform.position = new Vector3(-transform.position.x, transform.position.y, 0);
+            }
+        }
+        if (ObjectType == 6)
+        {
+            // The object is the health powerup.            
+            transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * 2.1f);
+
+            if (transform.position.x <= -9.8f)
+            {
+                // When it reaches the edge, go to the opposite side.
+                transform.position = new Vector3(-transform.position.x, transform.position.y, 0);
+            }
+        }
+
+        if (transform.position.y > 10f || transform.position.y < -10f)
+        {
+            // When it goes above or below the limit, it gets destroyed.
+            Destroy(this.gameObject);
+        }
+    }
+}
+
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,36 +11,40 @@ public class Player : MonoBehaviour
     private float horizontalScreenLimit = 10.38f;
     private float verticalScreenLimit = 4f;
     public int lives;
-    public TextMeshProUGUI livesText;
+    [HideInInspector] public TextMeshProUGUI lifeText;
+    [HideInInspector] public AudioSource coinPickup;
+    [HideInInspector] public AudioSource heartPickup;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerSpeed = 6f;  
+        playerSpeed = 6f;
         lives = 3;
-        livesText = GameObject.Find("GameManager").GetComponent<GameManager>().livesText;
-        livesText.text = "Lives: " + lives;
+        lifeText = GameObject.Find("GameManager").GetComponent<GameManager>().livesText;
+        lifeText.text = "Lives: " + lives;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
-        Shooting();        
-        
+        Shooting();
+
         //SHhhhhhhhhh this is a cheat for testing purposes.
         if (Input.GetKeyDown(KeyCode.L))
         {
             lives++;
-        }   
+            lifeText.text = "Lives: " + lives;
+        }
         //SHhhhhhhhhh this is a cheat for testing purposes.
         if (Input.GetKeyDown(KeyCode.K))
         {
             lives--;
-        }     
+            lifeText.text = "Lives: " + lives;
+        }
     }
 
-    void Movement ()
+    void Movement()
     {
         transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * playerSpeed);
 
@@ -63,7 +67,7 @@ public class Player : MonoBehaviour
 
     void Shooting()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(bulletPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
         }
@@ -72,13 +76,43 @@ public class Player : MonoBehaviour
     public void LoseLife()
     {
         lives--;
-        livesText.text = "Lives: " + lives;
-
+        //lives -= 1;
+        //lives = lives - 1;
+        lifeText.text = "Lives: " + lives;
         if (lives <= 0)
         {
             GameObject.Find("GameManager").GetComponent<GameManager>().GameOver();
             Instantiate(explosionPrefab, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
-    }    
+    }
+
+    void OnTriggerEnter2D(Collider2D whatIHit)
+    {
+        if (whatIHit.tag == "Coin")
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().EarnScore(1);
+            coinPickup = GameObject.Find("CoinAudio").GetComponent<AudioSource>();
+            coinPickup.Play();
+            Destroy(whatIHit.gameObject);
+        }
+        else if (whatIHit.tag == "Heart")
+        {
+            // Should we make the coin sound play when above 3 health because they earn 1 score? Or should we always play health sound? 
+            heartPickup = GameObject.Find("HeartAudio").GetComponent<AudioSource>();
+            heartPickup.Play();
+            Destroy(whatIHit.gameObject);
+
+            if (lives >= 3)
+            {
+                GameObject.Find("GameManager").GetComponent<GameManager>().EarnScore(1);
+            }
+            else if (lives < 3)
+            {
+                lives++;
+                lifeText.text = "Lives: " + lives;
+            }
+        }
+    }
 }
+
